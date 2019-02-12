7bfdbf46c270213b4dc3bcdda577bffabf364c36
Jason K. Frank <jasonkylefrank@gmail.com>
2019-02-12 11:17:41 -0500

    Started arranging the struture for the bullet chart and demo (app) components.

--- src/App.js
+++ src/App.js
@@ -1,26 +1,15 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.scss';
+import BulletChart from './components/BulletChart';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-          <p>Hello there man!</p>
+          Bullet chart demos
         </header>
+        <BulletChart testProp="123" />
       </div>
     );
   }
---
+++ src/components/BulletChart.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+export default function BulletChart(props) {
+  return (
+    <>
+        <div>
+        This is a bullet chart...
+        </div>
+        <div>
+            Your props are {props.testProp}
+        </div>
+    </>
+  )
+}
